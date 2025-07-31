f5282dc5fb63b9682da401c04b532db554834e62
JoaquinQFox <jquispebed@unsa.edu.pe>
2025-07-31 17:32:15 -0500

    feat: se crea clase y avance de objeto Task

---
+++ src/main/java/com/unsa/objects/Task.java
@@ -0,0 +1,60 @@
+package com.unsa.objects;
+
+import java.time.LocalDateTime;
+
+public class Task {
+    private static int globalId = 0;
+
+    private int id;
+    private String description;
+    private TaskStatus status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Task() {
+        this("No description");
+    }
+
+    public Task(String description) {
+        this.id = setGlobalId();
+        this.description = description;
+        this.status = TaskStatus.TO_DO;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    private int setGlobalId() {
+        globalId++;
+        return globalId;
+    }
+
+    public String toJson() {
+        return "{" +
+            "\"id\":" + getId() + "," + 
+            "\"description\":" + "\"" + getDescription() + "\"," +
+            "\"status\":" + "\"" + status.name() + "\"," +
+            "\"createdAt\":" + getCreatedAt() + "," +
+            "\"updatedAt\":" + getUpdatedAt() +
+            "}";
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public String getStatus() {
+        return status.getStatus();
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return this.createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return this.updatedAt;
+    }
+}
