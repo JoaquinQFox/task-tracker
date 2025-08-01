93db895476d7e426964febf61159440a727b4f73
JoaquinQFox <jquispebed@unsa.edu.pe>
2025-07-31 23:58:46 -0500

    test: se hace pruebas para crear carpeta y de output de json

--- src/main/java/com/unsa/tests/OutputTest.java
+++ src/main/java/com/unsa/tests/OutputTest.java
@@ -1,20 +1,28 @@
 package com.unsa.tests;
 
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 
 import com.unsa.objects.Task;
 
 public class OutputTest {
     public static void main(String[] args) {
+        String filePath = "jsonTest";
         Task task = new Task("Programar comandos");
 
-        try (PrintWriter writer = new PrintWriter("tasks.json", "UTF-8")){
-            writer.println(task.toJson() + "\n");
-        }
-        catch(FileNotFoundException | UnsupportedEncodingException e) {
-            e.getMessage();
+        try {
+            File folder = new File(filePath);
+            if (!folder.exists()) {
+                folder.mkdirs();
+            }
+
+            try (Writer fileWriter = new FileWriter("jsonTest/tasks.json")) {
+                fileWriter.write(task.toJson() + "\n");
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
