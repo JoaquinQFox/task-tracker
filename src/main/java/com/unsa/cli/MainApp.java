90cedab818329c9891c1e89b6e74c1b539b4ea73
JoaquinQFox <jquispebed@unsa.edu.pe>
2025-08-01 16:40:27 -0500

    feat: se completa lógica de clase MainApp

--- src/main/java/com/unsa/cli/MainApp.java
+++ src/main/java/com/unsa/cli/MainApp.java
@@ -5,6 +5,7 @@ import com.unsa.logic.TaskManager;
 public class MainApp {
     public static void main(String[] args) {
         TaskManager taskManager = new TaskManager();
+        taskManager.readTasks();
         taskManager.readCommand(args);
         taskManager.saveTasks();
     }
