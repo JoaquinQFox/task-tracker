6663bccec16e8c4875fefcaf3b9b6db995981648
JoaquinQFox <jquispebed@unsa.edu.pe>
2025-08-01 12:41:50 -0500

    feat: se crea método para manejar acción add en clase TaskManager

--- src/main/java/com/unsa/cli/TaskManager.java
+++ src/main/java/com/unsa/cli/TaskManager.java
@@ -1,16 +1,19 @@
 package com.unsa.cli;
 
-// import java.util.ArrayList;
-// import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
 
-// import com.unsa.objects.Task;
+import com.unsa.objects.Task;
 
 public class TaskManager {
-    // List<Task> tasks = new ArrayList<>();
+    static List<Task> tasks = new ArrayList<>();
     // String filePath = "jsonTest/tasks.json";
 
     public static void main(String[] args) {
+        System.out.println(args[0]);
+        System.out.println(args[1]);
         readAction(args);
+        System.out.println("List: " + tasks);
     }
 
     public static void readAction(String[] input) {
@@ -21,7 +24,7 @@ public class TaskManager {
         }
 
         switch (action) {
-            case "add" -> System.out.println("Task added succesfully (ID:1)");
+            case "add" -> addTask(input);
             case "update" -> System.out.println("Task updated succesfully (ID:1)");
             case "delete" -> System.out.println("Task deleted succesfully");
             case "list" -> System.out.println("Tasks listed");
@@ -29,7 +32,37 @@ public class TaskManager {
         }
     }
 
+    public static void addTask(String[] input) {
+        if (checkAddCommand(input)) {
+            String description = input[1];
+            Task task = new Task(description);
+
+            tasks.add(task);
+        }
+    }
+
+    public static boolean checkAddCommand(String[] input) {
+        if (input.length > 2) {
+            System.out.println("There must only be two arguments");
+            System.out.println("Example: add \"Your description\"");
+            return false;
+        }
+
+        if (input.length < 2) {
+            System.out.println("You gotta send the command with a description");
+            System.out.println("Description muste be encolsed with quotation marks if there are more than one word");
+            System.out.println("Example: add \"Your description\"");
+            return false;
+        }
+
+        return true;
+    }
+
     public static void readTasks() {
+
+    }
+
+    public static void saveTasks() {
 
     }
 }
